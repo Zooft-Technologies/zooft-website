32220296aa63c249e7c814508c60073b9777c6d5
Nahurira Clintonie <clintonnahurira24@gmail.com>
2026-02-17 15:47:14 +0300

    chore: remove unused files, dependencies and caches

--- website-frontend/src/App.tsx
+++ website-frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
@@ -9,10 +8,8 @@ import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
+  <>
     <Toaster />
     <BrowserRouter>
       <Routes>
@@ -22,11 +19,10 @@ const App = () => (
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  </QueryClientProvider>
+  </>
 );
 
 export default App;
